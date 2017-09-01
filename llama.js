a9c291e9a9dbe728118c732592dd42fcf1af43d3
Ilya Nikiporov <ilya.nikiporov@shopify.com>
2017-09-01 13:25:55 -0400

    Some context

--- llama.js
+++ llama.js
@@ -6,17 +6,17 @@ let x = document.getElementById('test');
 x.setAttribute("width", width);
 x.setAttribute("height", height);
 
-let fill = Math.round(width / 298);
+let fill = Math.round(width / 298); // How many tiles do we need to fill across the screen?
 console.log(fill);
 
 for (i=0;i<fill;i++){
 let a = 0;
-let dirt = new Image(298, 94);
+let dirt = new Image(); // Apparently we don't need anything here
 dirt.src = "Dirt.jpg";
 dirt.style.position="relative";
 dirt.style.left = a + "px";
 dirt.style.bottom = 0 + "px";
 document.body.appendChild(dirt);
-a+=298;
+a+=298; // Makes sure it tiles by moving the next drawn image by the width of the image
 }
      }
